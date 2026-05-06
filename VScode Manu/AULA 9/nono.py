ae5d040d84503ef38ae7f620ef811fbd44322364
Manu Rossatt <rossattmanuella@gmail.com>
2026-05-06 10:56:16 -0300

    Aula 6.5.2026

--- VScode Manu/AULA 9/nono.py
+++ VScode Manu/AULA 9/nono.py
@@ -7,7 +7,7 @@ print("COR: Vermelho (peça defeituosa)")
 print("COR: Verde (peça boa)")
 cor = input("Qual cor você deseja?")
 if cor == "Vermelho":
-    andar = int(input("Quantos passos deseja andar?"))
+    andar = int(input("Quantos passos deseja andar (de 0-66)?"))
     if andar == 0:
         print("Ficar parado")
     elif 0 < andar < 66:
@@ -16,19 +16,19 @@ if cor == "Vermelho":
         print("Agarrar peça Vermelha...")
         print("Levantar...")
         print("Agora volte...")
-        graus = int(input("Quantos graus você quer virar a direita? "))
+        graus = int(input("Quantos graus você quer virar a direita (de 0-176)? "))
         if graus == 0:
             print("Não gire.")
         elif 0 < graus < 176:
             print(f"Vire {graus}° a direita.")
-        graus2 = int(input("Quantos graus você quer virar a esquerda? "))
+        graus2 = int(input("Quantos graus você quer virar a esquerda (de 0-176)? "))
         if graus2 == 0:
             print("Não gire.")
         elif 0 < graus2 < 176:
             print(f"Vire {graus2}° a esquerda.")
         else:
             print("Encerrar sistema")
-        passos = int(input("Quantos passos você quer andar? "))
+        passos = int(input("Quantos passos você quer andar (de 0-66)? "))
         if passos == 0:
             print("Não andar")
         elif 0 < passos < 66:
@@ -37,7 +37,7 @@ if cor == "Vermelho":
         else:
             print("Encerrar sistema")
 elif cor == "Verde":
-    andar1 = int(input("Quantos passos deseja andar?"))
+    andar1 = int(input("Quantos passos deseja andar (de 0-36)?"))
     if andar1 == 0:
         print("Ficar parado")
     elif 0 < andar1 < 36:
@@ -46,19 +46,19 @@ elif cor == "Verde":
         print("Agarrar peça Verde...")
         print("Levantar...")
         print("Agora volte...")
-        graus3 = int(input("Quantos graus você quer virar a direita? "))
+        graus3 = int(input("Quantos graus você quer virar a direita (de 0-181)? "))
         if graus3 == 0:
             print("Não gire.")
         elif 0 < graus3 < 181:
             print(f"Vire {graus3}° a direita.")
-        graus4 = int(input("Quantos graus você quer virar a esquerda? "))
+        graus4 = int(input("Quantos graus você quer virar a esquerda (de 0-181)? "))
         if graus4 == 0:
             print("Não gire.")
         elif 0 < graus4 < 181:
             print(f"Vire {graus4}° a esquerda.")
         else:
             print("Encerrar sistema")
-        passos1 = int(input("Quantos passos você quer andar? "))
+        passos1 = int(input("Quantos passos você quer andar (de 0-36)? "))
         if passos1 == 0:
             print("Não andar")
         elif 0 < passos1 < 36:
