f1aac3276d98d0d29974998d7bc265ab47af9e97
Manu Rossatt <rossattmanuella@gmail.com>
2026-05-06 10:51:12 -0300

    Aula 6.5.2026

--- VScode Manu/AULA 7/setimo.py
+++ VScode Manu/AULA 7/setimo.py
@@ -30,38 +30,33 @@
 
 
 
-print("Bem-vindo ao Menu:")
-print("Formas de acesso")
-print("Opção 1: Emitir ticket")
-print("Opção 2: Verificar TAG")
-print("Opção 3: Interfone")
-acesso = int(input("Digite o número da opção desejada: "))
-placa = input("Digite a placa do veículo: ")
-modelo = input("Digite a marca e o modelo do carro")
+# print("Bem-vindo ao Menu:")
+# print("Formas de acesso")
+# print("Opção 1: Emitir ticket")
+# print("Opção 2: Verificar TAG")
+# print("Opção 3: Interfone")
+# acesso = int(input("Digite o número da opção desejada: "))
+# placa = input("Digite a placa do veículo: ")
+# modelo = input("Digite a marca e o modelo do carro")
 
 
-if acesso == 1:
-    print("Bem-vindo ao shopping")
-    valor_estacionamento = float(input("Digite o valor a ser cobrado por hora: "))
-    hora_de_entrada = float(input("Digite a hora de entrada: "))
-    hora_de_saida = float(input("Digite a hora de saída: "))
-    total_permanencia = hora_de_saida - hora_de_entrada
-    print(f"O tempo de permanência total em horas foi de: {total_permanencia} horas")
-    total_estacionamento = total_permanencia * valor_estacionamento
-    print(f"O valor a ser cobrado foi de {total_estacionamento:.2f}")
-    print("devolver o ticket")
-elif acesso == 2:
-    print("Bem-vindo ao shopping")
-    print(f"O valor cobrado por hora pelo estacionamento é {valor_estacionamento}")
-elif acesso == 3:
-    print("Acionando interfone")
-    print("Bem-vindo ao shopping")
-    print("Liberando acesso pelo Interfone")
-    print("Não esqeuça que sua saída deverá ser realiza também pelo Inteforne")
-else:
-    print("Encerrando sistema.")
-
-print("_________________________________")
-print(" Relatório de entradas e saídas")
-print("_________________________________")
-carro1 = 
+# if acesso == 1:
+#     print("Bem-vindo ao shopping")
+#     valor_estacionamento = float(input("Digite o valor a ser cobrado por hora: "))
+#     hora_de_entrada = float(input("Digite a hora de entrada: "))
+#     hora_de_saida = float(input("Digite a hora de saída: "))
+#     total_permanencia = hora_de_saida - hora_de_entrada
+#     print(f"O tempo de permanência total em horas foi de: {total_permanencia} horas")
+#     total_estacionamento = total_permanencia * valor_estacionamento
+#     print(f"O valor a ser cobrado foi de {total_estacionamento:.2f}")
+#     print("devolver o ticket")
+# elif acesso == 2:
+#     print("Bem-vindo ao shopping")
+#     print(f"O valor cobrado por hora pelo estacionamento é {valor_estacionamento}")
+# elif acesso == 3:
+#     print("Acionando interfone")
+#     print("Bem-vindo ao shopping")
+#     print("Liberando acesso pelo Interfone")
+#     print("Não esqeuça que sua saída deverá ser realiza também pelo Inteforne")
+# else:
+#     print("Encerrando sistema.")
